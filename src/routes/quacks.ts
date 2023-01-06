087f331c1bc5691b5cebd868940af83338a8baec
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2023-01-06 12:59:21 +1100

    fix

--- src/routes/quacks.ts
+++ src/routes/quacks.ts
@@ -49,48 +49,36 @@ router.post(
   },
 );
 
-//Like a quack
+//Like or un-like a quack
 router.patch(
   '/api/user/:username/quacks/:id',
   async (req: Request, res: Response) => {
     try {
-      await Quack.findOneAndUpdate(
-        { _id: req.params.id },
-        { $addToSet: { likes: req.body.likedUsername } },
-        { returnDocument: 'after' },
-      );
-      res.status(200).send({
-        success: true,
-        message: 'Quack liked',
-      });
+      if (req.body.liked) {
+        await Quack.findOneAndUpdate(
+          { _id: req.params.id },
+          { $addToSet: { likes: req.body.likedUsername } },
+          { returnDocument: 'after' },
+        );
+        res.status(200).send({
+          success: true,
+          message: 'Quack liked',
+        });
+      } else {
+        await Quack.findOneAndUpdate(
+          { _id: req.params.id },
+          { $pull: { likes: req.body.likedUsername } },
+          { returnDocument: 'after' },
+        );
+        res.status(200).send({
+          success: true,
+          message: 'Quack un-liked',
+        });
+      }
     } catch (error) {
       res.status(404).send({
         success: false,
-        message: 'Could not like quack',
-        error,
-      });
-    }
-  },
-);
-
-//Un-like a quack
-router.patch(
-  '/api/user/:username/quacks/:id',
-  async (req: Request, res: Response) => {
-    try {
-      await Quack.findOneAndUpdate(
-        { _id: req.params.id },
-        { $pull: { likes: req.body.likedUsername } },
-        { returnDocument: 'after' },
-      );
-      res.status(200).send({
-        success: true,
-        message: 'Quack un-liked',
-      });
-    } catch (error) {
-      res.status(404).send({
-        success: false,
-        message: 'Could not un-like quack',
+        message: 'Failed to update quack status',
         error,
       });
     }
