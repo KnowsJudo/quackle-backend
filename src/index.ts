121bdaf432cf6ddf94eaec0d879b800fc49adfae
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2023-01-27 00:33:12 +1100

    envs

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,18 @@ import { quackRouter } from './routes/quacks';
 import { userRouter } from './routes/user';
 import { createLog } from './middleware/log';
 import { flockRouter } from './routes/flock';
+import * as dotenv from 'dotenv';
+import path from 'path';
 
+try {
+  dotenv.config({
+    path: path.join(__dirname, `../.env.${process.env.NODE_ENV}`),
+  });
+} catch (error) {
+  console.log('Could not load env variables');
+}
+
+const mongoCred = process.env.MONGO_URI;
 const port = 3001;
 const app = express();
 
@@ -30,7 +41,9 @@ app.use(quackRouter);
 app.use(flockRouter);
 
 try {
-  mongoose.connect('mongodb://localhost:27017/quackle');
+  // mongoose.connect('mongodb://localhost:27017/quackle');
+  mongoose.connect(mongoCred as string);
+
   console.log('connected to database');
 } catch (e) {
   console.error('error connecting to database', e);
