eef0675ecb489cdd93084c169c1f67fe1a1d4cff
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2023-02-04 16:28:01 +1100

    regex

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -41,6 +41,7 @@ const defaultFilter = new Filter();
 const regex = /\b([aA4][bB][ou0]{0,2}|[bB]00ng|[bB]o[o0]ng)(?:[sS5]{0,2})\b/;
 const emailRegex =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])+(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])+)+$/;
+const userRegex = /^[^a-zA-Z]/;
 
 /* List user or multiple users
  * @requiresAuth: false
@@ -122,6 +123,7 @@ router.post('/api/user', async (req: Request, res: Response) => {
     regex.test(name) ||
     defaultFilter.isProfane(username) ||
     regex.test(username) ||
+    userRegex.test(username) ||
     !emailRegex.test(email)
   ) {
     return res
