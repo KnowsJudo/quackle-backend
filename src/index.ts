f93d5c3d5f5622a3208eb49e4ce9c46c1e983463
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2024-02-26 11:20:30 +1100

    test

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,9 @@ const port = 3001;
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Origin', 'https://www.quackle.net');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
